107fc6f96f413462231b1a7bb6df7dc5eced2209
Darshana Welikala <darshanadsw@gmail.com>
2019-02-10 21:47:41 -0500

    added chuknorris bean to java config

---
+++ src/main/java/guru/springframwork/jokes/app/jokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,18 @@
+package guru.springframwork.jokes.app.jokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Darshana Welikala on 2019-02-10.
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
